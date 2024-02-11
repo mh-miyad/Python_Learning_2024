e204e782b48ab012a5b855740ca6bfbbdbee5592
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2024-02-11 07:48:48 +0600

    number part simply done

--- Number/Number.py
+++ Number/Number.py
@@ -16,6 +16,23 @@
 # x,y,z = 1,2,3
 
 # print(x,y,z)
-import math
-x = math.floor(3.8)
+# import math
+# x = math.floor(3.8)
+# print(x)
+
+# import random
+# x = random.choice([1, 2, 3, 4, 5, 6])
+# print(x)
+
+# from decimal import Decimal
+
+# x = Decimal(3.14)
+# y = Decimal(23.1445454545)
+
+# z = x + y
+# print(z)
+
+setOne = {1, 2, 3, 4}
+setTwo = {3, 4, 5, 6}
+x = setOne | setTwo  # intersection
 print(x)
