b4325cc0fa7ebf530dde5522d7c12d5f1054f8aa
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2024-02-11 07:39:10 +0600

    math

---
+++ Number/Number.py
@@ -0,0 +1,21 @@
+"""
++,_,*,/,//,%,**
+
+"""
+
+# example 1
+# x = 3
+# y = 5
+# z = x ** y
+# c=  (x * y) + z
+# b = input()
+# print("==================================")
+# print(b)
+
+# example 2 tuple
+# x,y,z = 1,2,3
+
+# print(x,y,z)
+import math
+x = math.floor(3.8)
+print(x)
